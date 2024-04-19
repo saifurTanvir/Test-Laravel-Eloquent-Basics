ac45ad22e0a9b0dbbc5a13e49bf1e397732aef92
Md. Saifur Rahman <msaifur@praava.net>
2024-04-19 12:38:48 +0600

    all test passed

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,11 +2,16 @@
 
 namespace App\Models;
 
+use App\Observers\ProjectObserver;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+#[ObservedBy([ProjectObserver::class])]
 class Project extends Model
 {
     use HasFactory, SoftDeletes;
+
+    protected $guarded = [];
 }
